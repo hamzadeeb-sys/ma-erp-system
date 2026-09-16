a5426638ba1fdc12507daf0673f38db04c492950
Hamza Omar Deeb <hamzawisti@gmail.com>
2026-09-16 14:06:26 +0300

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ma-erp-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/app/static/manifest.json',
+  '/app/static/icon-192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
